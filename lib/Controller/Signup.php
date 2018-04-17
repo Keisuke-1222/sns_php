c503f5f7718330655173653b2866b5370ded10c5
vagrant <vagrant@localhost.localdomain>
2018-04-17 10:08:39 +0900

    create signup.php

---
+++ lib/Controller/Signup.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MyApp\Controller;
+
+class Signup extends \MyApp\Controller {
+
+  public function run() {
+    if ($this->isLoggedIn()) {
+      header('Location: ' . SITE_URL);
+      exit;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $this->postProcess();
+    }
+  }
+
+
+
+}
